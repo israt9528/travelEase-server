777d97d0e0d1c19cf1f66e530e01455ae4f77be1
israt <israt9528@gmail.com>
2025-11-10 04:00:02 +0600

    implement find method to get data from database

--- index.js
+++ index.js
@@ -1,11 +1,57 @@
 const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri =
+  "mongodb+srv://travelEaseUser:ecFps93npSS8gjPC@cluster0.u1z8wkz.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const db = client.db("travelEase_db");
+    const vehicleCollection = db.collection("travel-collection");
+
+    app.get("/vehicles", async (req, res) => {
+      const result = await vehicleCollection
+        .find()
+        .sort({ pricePerDay: -1 })
+        .toArray();
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("travelEase server is running now!");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`travelEase server is listening on port ${port}`);
 });
