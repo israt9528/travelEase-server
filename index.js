31d4eadbf38cce5ee971031718cfef2de85780a5
israt <israt9528@gmail.com>
2025-11-10 23:36:56 +0600

    implement post method to post new data into database

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+const admin = require("firebase-admin");
+
+const serviceAccount = require("./serviceKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
 app.use(cors());
 app.use(express.json());
@@ -20,6 +28,30 @@ const client = new MongoClient(uri, {
   },
 });
 
+const verifyToken = async (req, res, next) => {
+  const authorization = req.headers.authorization;
+
+  if (!authorization) {
+    res.status(401).send({
+      message: "unauthorized access: token not found",
+    });
+  }
+
+  const token = authorization.split(" ")[1];
+
+  try {
+    const decode = await admin.auth().verifyIdToken(token);
+    console.log("✅ Token verified:", decode.email);
+
+    next();
+  } catch (error) {
+    console.error("❌ Token verification failed:", error.message);
+    res.status(401).send({
+      message: "unauthorized access",
+    });
+  }
+};
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -33,6 +65,19 @@ async function run() {
         .find()
         .sort({ pricePerDay: -1 })
         .toArray();
+      res.send(result);
+    });
+
+    app.post("/vehicles", verifyToken, async (req, res) => {
+      const data = req.body;
+      const result = await vehicleCollection.insertOne(data);
+      res.send(result);
+    });
+
+    app.get("/vehicles/:id", async (req, res) => {
+      const { id } = req.params;
+      const objectId = new ObjectId(id);
+      const result = await vehicleCollection.findOne({ _id: objectId });
       res.send(result);
     });
 
