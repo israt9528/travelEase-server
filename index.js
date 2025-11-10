74356617a5237938268baaec86ee731c31456a76
israt <israt9528@gmail.com>
2025-11-10 23:54:06 +0600

    secure private key and info with dot-env

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -16,8 +16,7 @@ admin.initializeApp({
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  "mongodb+srv://travelEaseUser:ecFps93npSS8gjPC@cluster0.u1z8wkz.mongodb.net/?appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.u1z8wkz.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
